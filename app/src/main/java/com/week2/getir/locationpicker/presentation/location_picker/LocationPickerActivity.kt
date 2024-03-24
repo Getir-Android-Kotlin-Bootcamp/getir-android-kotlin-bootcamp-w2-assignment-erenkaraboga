dfdca9efa381514a54a68fa3886d2b8e581377d7
erenkaraboga <eren.karaboga@naylalabs.com>
2024-03-25 00:57:12 +0300

    Add google map service

--- app/src/main/java/com/week2/getir/locationpicker/presentation/location_picker/LocationPickerActivity.kt
+++ app/src/main/java/com/week2/getir/locationpicker/presentation/location_picker/LocationPickerActivity.kt
@@ -4,17 +4,28 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.week2.getir.locationpicker.R
 import com.week2.getir.locationpicker.databinding.ActivityLocationPickerBinding
 
-class LocationPickerActivity : AppCompatActivity() {
+class LocationPickerActivity : AppCompatActivity(),OnMapReadyCallback {
     private lateinit var binding: ActivityLocationPickerBinding
+    private  var map : GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLocationPickerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initMapView()
     }
-
+    private fun initMapView(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+    }
     companion object {
         fun createSimpleIntent(context: Context): Intent =
             Intent(context, LocationPickerActivity::class.java)
